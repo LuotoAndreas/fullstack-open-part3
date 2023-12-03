333022459b0cb6525049853771697c72b645b530
LuotoAndreas <152082632+LuotoAndreas@users.noreply.github.com>
2023-12-03 20:02:44 +0200

    added dist

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'));
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('reqBody', (req, res) => {
   return JSON.stringify(req.body)
