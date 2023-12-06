0324cc0f444b910b6c9eb33a0d598be709e5cd84
LuotoAndreas <152082632+LuotoAndreas@users.noreply.github.com>
2023-12-07 01:43:39 +0200

    Almost final push for part3

--- index.js
+++ index.js
@@ -5,24 +5,10 @@ const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'));
-app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 app.use(cors())
+app.use(express.json())
 app.use(express.static('dist'))
-app.use(errorHandler)
-
 
 morgan.token('reqBody', (req, res) => {
   return JSON.stringify(req.body)
@@ -42,7 +28,7 @@ app.get('/api/persons/:id', (request, response, next) => {
       response.status(400).end()
     }
   })
-  .catch((error) => next(error))
+    .catch((error) => next(error))
 })
   
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -54,7 +40,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number} = request.body
+  const { name, number } = request.body
 
   Person.findByIdAndUpdate(
     request.params.id,
@@ -75,10 +61,11 @@ app.post('/api/persons', (request, response, next) => {
     number: body.number,
   })
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
+  person.save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -92,7 +79,20 @@ app.get('/info', (request, response) => {
     })
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-}) 
+})
